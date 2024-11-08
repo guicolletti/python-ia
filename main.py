0ac8267b10d5f9709ca1f8617f076065211fff42
guicolletti <165791501+guicolletti@users.noreply.github.com>
2024-11-08 09:32:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,26 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Rota para renderizar a página principal com o chatbot
+@app.route('/')
+def index():
+    return render_template('index.html')  # Certifique-se de ter a página 'index.html' em 'templates/'
 
+# Rota para buscar resposta e vídeos do chatbot
 @app.route('/search')
 def search():
     model = genai.GenerativeModel('gemini-1.0-pro-latest')
     genai.configure(api_key=os.getenv('API'))
-    
+
     # Contexto da IA
     context = ('Você é um chat destinado a responder, guiar e ensinar jovens aprendizes a como utilizar os mais diversos automóveis do agro, como uma colheitadeira, etc. Seu nome é CNHbot.')
-    
+
     # Pega o prompt (mensagem do usuário)
     prompt = request.args.get('prompt')
-    
+
     # Expressão regular para detectar URL do YouTube e pegar o video_id
-    youtube_url_pattern = r'(https?://www\.youtube\.com/watch\?v=([a-zA-Z0-9_-]+))'
-    
+    youtube_url_pattern = r'(https?://(?:www\.youtube\.com/watch\?v=|youtu\.be/)([a-zA-Z0-9_-]+))'
+
     # Verificar se o prompt contém um link do YouTube
     match = re.search(youtube_url_pattern, prompt)
     if match:
@@ -30,11 +35,11 @@ def search():
         video_response = f"<iframe width='560' height='315' src='https://www.youtube.com/embed/{video_id}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
     else:
         video_response = None
-    
+
     # Gerar resposta com a IA
     input_ia = f'{context}: {prompt}'
     output = model.generate_content(input_ia)
-    
+
     return {'message': output.text, 'video': video_response}
 
 if __name__ == '__main__':
